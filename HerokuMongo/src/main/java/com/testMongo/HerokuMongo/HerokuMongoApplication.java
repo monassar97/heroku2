82ec5f8cb719460ffca20f1ff1a022e0698bbce0
monassar97 <mo.nassar97@gmail.com>
2020-07-29 14:17:36 +0300

    asd

---
+++ HerokuMongo/src/main/java/com/testMongo/HerokuMongo/HerokuMongoApplication.java
@@ -0,0 +1,13 @@
+package com.testMongo.HerokuMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuMongoApplication.class, args);
+	}
+
+}
---
+++ HerokuMongo/src/test/java/com/testMongo/HerokuMongo/HerokuMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testMongo.HerokuMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
